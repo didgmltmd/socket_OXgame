d8eb73cae2e0edbeae94f9f9d3f9d06da9433db3
didgmltmd <gomboy11@naver.com>
2025-11-01 19:57:41 +0900

    fix: 백엔드 주소 변경

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,9 +1,12 @@
 import { io, Socket } from "socket.io-client";
 
 // 서버 주소는 Vite 환경변수로 관리
-export const socket: Socket = io("https://socket-oxgame-server.onrender.com", {
-  autoConnect: true,
-});
+export const socket: Socket = io(
+  "https://socket-oxgame-server-1.onrender.com",
+  {
+    autoConnect: true,
+  }
+);
 
 socket.on("connect", () => console.log("socket connected", socket.id));
 socket.on("connect_error", (err) => console.error("connect_error", err));
